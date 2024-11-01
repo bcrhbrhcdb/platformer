6dc0ffd0a1b88e044b216aedf2ba966fc07c6a74
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-31 21:48:02 -0500

    njhfn

---
+++ index.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+canvas.width = 64 * 16; // 1024
+canvas.height = 64 * 9; // 576
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+let y = 100;
+const height = 100;
+let bottom = y + 100;
+function animate(){
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'red';
+    c.fillRect(100, y, 100, height);
+    if(bottom < canvas.height){
+    y += 5
+     bottom = y + 100;
+
+    }
+}
+animate()
